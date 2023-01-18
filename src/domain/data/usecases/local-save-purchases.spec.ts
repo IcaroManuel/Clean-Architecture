12bccab2400b3ac142f8598c6e70314a05e51d2c
IcaroManuel <icaroomanuel@gmail.com>
2023-01-18 16:28:25 -0300

    add: localDeletePurchases and localSavePurchases on cache

---
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -0,0 +1,35 @@
+class LocalSavePurchases {
+  constructor(private readonly cacheStore: CacheStore) {}
+  async save(): Promise<void> {
+    this.cacheStore.delete()
+  }
+}
+interface CacheStore {
+  delete: ( ) => void
+}
+
+class CacheStoreSpy implements CacheStore  {
+  deleteCallsCount = 0
+  
+  delete (): void {
+    this.deleteCallsCount++
+  }
+}
+
+describe('LocalSavePurchases', () => {
+  test('should not delete cache on sut.init', () => {
+    const cacheStore = new CacheStoreSpy()
+    new LocalSavePurchases(cacheStore)
+    expect(cacheStore.deleteCallsCount).toBe(0)
+  })
+  
+  test('should delete old cache on sut.init', async () => {
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalSavePurchases(cacheStore)
+    await sut.save()
+    expect(cacheStore.deleteCallsCount).toBe(1)
+  })
+})
+
+
+  
