15c2a062feb81b36f690cef3aad2d0ac10801eab
IcaroManuel <icaroomanuel@gmail.com>
2023-01-21 18:48:16 -0300

    create: Test LocalLoadPurchase

---
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -0,0 +1,26 @@
+
+import { CacheStoreSpy } from "@/data/tests"
+import { LocalLoadPurchases } from "./local-load-purchases"
+
+type SutTypes = {
+  sut: LocalLoadPurchases
+  cacheStore: CacheStoreSpy
+}
+const makeSut = (timestamp =  new Date()): SutTypes => {
+  const cacheStore = new CacheStoreSpy()
+    const sut = new LocalLoadPurchases(cacheStore, timestamp)
+    return {
+      sut,
+      cacheStore
+    }
+}
+
+
+describe('LocalLoadPurchases', () => {
+  test('should not delete or insert cache on sut.init', async () => {
+    const { cacheStore } = makeSut()
+    expect(cacheStore.actions).toEqual([])
+  })
+  
+  
+})
