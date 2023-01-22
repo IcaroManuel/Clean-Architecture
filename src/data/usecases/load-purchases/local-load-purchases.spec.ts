5759cb649b8386f89c3f0d8cfd9afca1366f8388
IcaroManuel <icaroomanuel@gmail.com>
2023-01-22 00:49:59 -0300

    add: fail cases tests

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -29,4 +29,13 @@ describe('LocalLoadPurchases', () => {
     expect(cacheStore.fetchKey).toBe('purchases')
   })
   
+  test('should return empty list if load fails', async () => {
+    const { cacheStore, sut } = makeSut()
+    cacheStore.simulateFetchError()
+    const purchases = await sut.loadAll()
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete])
+    expect(cacheStore.deleteKey).toBe('purchases')
+    expect(purchases).toEqual([])
+    })
+  
 })
